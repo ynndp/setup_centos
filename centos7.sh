405124dc3e0666f00e400e74eba532950a3ba312
ynndp <yanuarnnd10@gmail.com>
2023-11-03 10:32:48 +0700

    first commit

---
+++ centos7.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+##COMMAND###
+timedatectl set-timezone Asia/Jakarta
+yum update -y
+yum install firewalld
+systemctl start firewalld
+systemctl enable firewalld
+firewall-cmd --zone=public --add-service=http --permanent && firewall-cmd --zone=public --add-service=https --permanent
+firewall-cmd --reload
+yum -y install epel-release
+rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+yum install nginx
+setsebool httpd_can_network_connect on -P
+
+
+##STATUS###
+clear
+echo "SETUP VPS CENTOS 7" | tail -f
+echo "===============================================" 
